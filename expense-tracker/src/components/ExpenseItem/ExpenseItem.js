b7e662b1564123a3ca1f7015e90fd90e2e49be5f
Itai Kumengisa <imehku222@gmail.com>
2022-09-13 13:44:11 -0500

    extracting out a card component

--- expense-tracker/src/components/ExpenseItem/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem/ExpenseItem.js
@@ -1,14 +1,15 @@
 import './ExpenseItem.css'
-import ExpenseDate from './ExpenseDate/ExpenseDate';
+import ExpenseDate from '../ExpenseDate/ExpenseDate';
+import Card from '../Card/Card';
 const ExpenseItem = ({date, amount, title}) => {
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={date}/>
             <div className="expense-item__description">
                 <h2>{title}</h2>
                 <div className="expense-item__price">{`$${amount.toFixed(2)}`}</div>
             </div>
-        </div>);
+        </Card>);
 }
 
 
